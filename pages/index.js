c06f16dfb7d98757d0157807e5a5806198f5a65b
Kent Miguel <kentmiguel10@gmail.com>
2023-02-08 12:47:04 +0100

    Update UI

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
-import Image from "next/image"
 import styles from "../styles/Home.module.css"
+import { useMoralisQuery, useMoralis } from "react-moralis"
+import NFTBox from "../components/NFTBox"
+import networkMapping from "../constants/networkMapping.json"
+import GET_ACTIVE_ITEMS from "../constants/subgraphQueries"
+import { useQuery } from "@apollo/client"
 
 export default function Home() {
-    //How do we show the recently listed nfts?
-    //we will index the events off chain and read them from our database
-    //set up a server to listen for those events to be fired, and we will add them to a database query
+    const { isWeb3Enabled, chainId } = useMoralis()
+    const chainString = chainId ? parseInt(chainId).toString() : "31337"
+    const marketplaceAddress = networkMapping[chainString].NftMarketplace[0]
 
-    return <div className={styles.container}>hi!</div>
+    const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS)
+
+    return (
+        <div className="container mx-auto">
+            <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+            <div className="flex flex-wrap">
+                {isWeb3Enabled ? (
+                    loading || !listedNfts ? (
+                        <div>Loading...</div>
+                    ) : (
+                        listedNfts.activeItems.map((nft) => {
+                            console.log(nft)
+                            const { price, nftAddress, tokenId, seller } = nft
+                            return (
+                                <NFTBox
+                                    price={price}
+                                    nftAddress={nftAddress}
+                                    tokenId={tokenId}
+                                    marketplaceAddress={marketplaceAddress}
+                                    seller={seller}
+                                    key={`${nftAddress}${tokenId}`}
+                                />
+                            )
+                        })
+                    )
+                ) : (
+                    <div>Web3 Currently Not Enabled</div>
+                )}
+            </div>
+        </div>
+    )
 }
